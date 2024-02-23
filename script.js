954b46a2551905029660e910ab603339388fec48
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-23 23:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+document.querySelector("form").addEventListener("submit", function(){
+	alert(`First Name: ${first.value} Last Name: ${Last.value} Phone Number : ${num.value} Email ID : ${email.value}`);
+})
