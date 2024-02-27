d31fc11536956ac7e4b562ab3473931e9fd20350
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-27 18:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //your JS code here. If required.
+const data = {};
 document.querySelector("form").addEventListener("submit", function(event){
 
-			 event.preventDefault();
+	 event.preventDefault();
 			
-	const firstName = document.getElementById("first").value;
-	const LastName = document.getElementById("Last").value;
-	const phoneNo = document.getElementById("num").value;
-	const email = document.getElementById("email").value;
+	data.firstName = document.getElementById("first").value;
+    data.LastName = document.getElementById("Last").value;
+	data.phoneNo = document.getElementById("num").value;
+	data.email = document.getElementById("email").value;
 
-	const alertMessage = `First Name : ${firstName} Last Name : ${LastName} Phone Number: ${phoneNo} Email ID : ${email}`;
+	const alertMessage = `First Name : ${data.firstName} Last Name : ${data.LastName} Phone Number: ${data.phoneNo} Email ID : ${data.email}`;
 	
 	alert(alertMessage);
 })
